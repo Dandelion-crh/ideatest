f3eeb76a4d9be398f236dfd34ba527e96805c824
crh <729532543@qq.com>
2020-04-17 10:16:09 +0800

    firstcommit

---
+++ gittest/src/main/java/pojo/User.java
@@ -0,0 +1,27 @@
+package pojo;
+
+/**
+ * 类名: User
+ * 作者: crh
+ * 日期: 2020/4/17 0017上午 10:12
+ */
+public class User {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
